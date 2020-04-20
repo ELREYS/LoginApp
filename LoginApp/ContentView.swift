7bd7051c5de2e77d88f5733aac5b94361838aed8
Giuseppe Di Lisa <gdilisa@gditdev.ch>
2020-04-20 08:08:50 -0700

    Finished App

--- LoginApp/ContentView.swift
+++ LoginApp/ContentView.swift
@@ -14,31 +14,56 @@ let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255
 
 struct ContentView: View {
     
+    let storedUsername = "Bob"
+    let storedPassword = "Marley"
+    
     @State var userName = ""
     @State var password = ""
+    @State var authenticationDidFail:Bool  = false
+    @State var authenticationDidSucced:Bool  = false
+    @State var editingMode: Bool = false
+    
     var body: some View {
-        VStack {
-            WelcomeText()
-            UserImage()
+        ZStack{
             VStack {
-                TextField("Username", text:$userName)
-                    .padding()
-                    .background(lightGreyColor)
-                    .cornerRadius(5.0)
-                    .padding(.bottom,20)
-                SecureField("Password", text:$password)
-                    .padding()
-                    .background(lightGreyColor)
-                    .cornerRadius(5.0)
-                    .padding(.bottom,20)
-                Button(action: {print("Login Button clicked")}){
-                LoginButtonContent()
+                WelcomeText()
+                UserImage()
+                if authenticationDidSucced{
+                    Text("Login successful!")
+                        .font(.headline)
+                    .frame(width: 250, height: 80)
+                        .background(Color.green)
+                        .cornerRadius(20.0)
+                        .foregroundColor(.white)
+                        .animation(Animation.easeOut)
+                    
                 }
+                VStack {
+                    UsernameTextField(userName: $userName,editingMode: $editingMode)
+                    PasswordSecureField(password: $password)
+                    if authenticationDidFail{
+                        Text("Information not correctly. Try again.")
+                            .offset(y:-10)
+                            .foregroundColor(.red)
+                    }
+                    Button(action: {
+                        if self.userName == self.storedUsername && self.password == self.storedPassword{
+                            self.authenticationDidFail = false
+                            self.authenticationDidSucced = true
+                        }
+                        else{
+                            self.authenticationDidFail = true
+                        }
+                    }){
+                        LoginButtonContent()
+                    }
+                    
+                }.padding()
                 
-            }.padding()
-            
+            }
             
         }
+        .offset(y:editingMode ? -150:0)
     }
 }
 
@@ -77,5 +102,41 @@ struct LoginButtonContent: View {
             .frame(width: 220, height: 60)
             .background(Color.green)
             .cornerRadius(15.0)
+    }
+}
+
+struct UsernameTextField: View {
+    
+    @Binding var userName:String
+    @Binding var editingMode: Bool
+    
+    var body: some View {
+        TextField("Username", text:$userName,onEditingChanged: {
+            edit in if edit{
+                self.editingMode = true
+                
+            }
+            else{
+                self.editingMode = false
+            }
+        })
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom,20)
+    }
+}
+
+struct PasswordSecureField: View {
+    
+    @Binding var password:String
+    @Binding var editingMode: Bool
+    
+    var body: some View {
+        SecureField("Password", text:$password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom,20)
     }
 }
